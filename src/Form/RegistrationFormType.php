af73393fa654d0ff0fb3acd85954d3aa060204c7
Bruno <mct.bruno@gmail.com>
2023-03-06 20:40:00 +0100

    Connexion and registration system

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, ['attr' => ['class' => 'form-control mt-2'], 'label' => 'Mon E-mail'])
+            ->add('lastname', TextType::class, ['attr' => ['class' => 'form-control mt-2'], 'label' => 'Mon nom'])
+            ->add('firstname', TextType::class, ['attr' => ['class' => 'form-control mt-2'], 'label' => 'Mon prénom'])
+            ->add('address', TextType::class, ['attr' => ['class' => 'form-control mt-2'], 'label' => 'Mon adresse'])
+            ->add('zipcode', TextType::class, ['attr' => ['class' => 'form-control mt-2'], 'label' => 'Mon code-postal'])
+            ->add('city', TextType::class, ['attr' => ['class' => 'form-control mt-2'], 'label' => 'Mon ville'])
+            ->add('RGPDConsent', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'En m\'inscrivant, j\'accépte que les règles d\'utilisations',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'label' => 'Mon mot de passe',
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'class' => 'form-control mt-2'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
